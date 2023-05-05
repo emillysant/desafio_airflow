be5853351b694acd9ddb10e0edd7db7f339c4c87
Pamela Nunes <pamela.nunes@indicium.tech>
2023-05-05 16:28:20 -0300

    Updated the airflow version for run install.sh without problem with the url

--- install.sh
+++ install.sh
@@ -8,7 +8,6 @@ PYTHON_VERSION="$(python --version | cut -d " " -f 2 | cut -d "." -f 1-2)"
 # For example: 3.6
 CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt"
 
-# For example: https://raw.github
 # For example: https://raw.githubusercontent.com/apache/airflow/constraints-2.2.3/constraints-3.6.txt
 pip install "apache-airflow==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}"
 
